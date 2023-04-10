74016d06e73e7b6dd2c9766783a507f1cc7c14e5
Agustin Cabrera <58793527+AgustinCabrera@users.noreply.github.com>
2023-04-10 16:08:54 -0300

    PreEntrega1+Cabrera

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import { useState } from 'react';
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
     <NavBar />
+    <ItemListContainer greeting={"Welcome"} />
     </div>
   );
 }
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const itemListContainer = ({ greeting }) => {
+  return (
+    <div>
+        <h2>{greeting}</h2>
+    </div>
+  )
+}
+export default itemListContainer
